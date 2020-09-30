1422afa55bccb80c417e0f6802ddd45b8b1b062a
Misael Santana <misaelsantana2011@hotmail.com>
2020-09-29 21:12:50 -0300

    att

--- exercicioaula-bubblesort/bubblesort.py
+++ exercicioaula-bubblesort/bubblesort.py
@@ -35,6 +35,8 @@ vetor = [20, 30, 90, 45, 10, 6, 8, 40, 25, 36, 98, 102, 360, 1]
 bubble = BubbleSort()
 bubble.ordenaCrescente(vetor)
 print(vetor)
+bubble.ordenaDecrescente(vetor)
+print(vetor)
 
 
 # visualgo.net 
