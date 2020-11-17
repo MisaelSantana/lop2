e4d2058fefce8705c31a42eaa203e9bd0a08e55f
MisaelSantana <misaelsantana2011@hotmail.com>
2020-11-17 17:20:50 -0300

    adicao do trabalho arquivo.log python

--- arquivos/Manipulacao_arquivos.py
+++ arquivos/Manipulacao_arquivos.py
@@ -1,47 +1,71 @@
 class Arquivo:
-    def abre_arquivo(self, nome, modo):
-        self.arquivo_leitura = open(nome, modo)
+    def __init__(self):
+        self.vetor = []
+        self.vetor2 = []
+        self.vetor_setosa = []
+        self.vetor_versicolor = []
+        self.vetor_virginica = []
+    
+    def abre_arquivo(self):
+        self.arquivo = open('iris.data', 'rt')
 
-    def mostrar_linhas(self):
-        for linha in self.arquivo_leitura:
-            print(linha)
-
-    def fecha_arquivo(self):
-        self.arquivo_leitura.close()
-
-    def contar_setosa(self):
-        find_setosa = self.arquivo_leitura.find('Iris-setosa')
-        qtd_setosa = 0
-        if(find_setosa >= 1):
-            qtd_setosa += 1
+    def transformar_em_vetor(self):
+        for linha in self.arquivo:
+            self.vetor.append(linha.replace('\n', '')[16:])
+            self.vetor2.append(linha.replace('\n', '')[0:3])
+        for i in range(50):
+            self.vetor_setosa.append(self.vetor2[i])
+        self.vetor_setosa.sort()
+        for i in range(50, 100):
+            self.vetor_versicolor.append(self.vetor2[i])
+        self.vetor_versicolor.sort()
+        for i in range(100, 150):
+            self.vetor_virginica.append(self.vetor2[i])
+        self.vetor_virginica.sort()
             
+    def contar_setosa(self):
+        self.qtd_setosas = self.vetor.count('Iris-setosa')
+
     def contar_versicolor(self):
-        find_versicolor = self.arquivo_leitura.find('Iris-versicolor')
-        qtd_versicolor = 0
-        if(find_versicolor >= 1):
-            qtd_versicolor += 1
+        self.qtd_versicolor = self.vetor.count('Iris-versicolor')
 
     def contar_virginica(self):
-        find_virginica = self.arquivo_leitura.find('Iris-virginica')
-        qtd_virginica = 0
-        if(find_virginica >= 1):
-            qtd_virginica += 1
+        self.qtd_virginica = self.vetor.count('Iris-virginica')
 
-    def abrir_saida(self, nome, mode):
-        self.arquivo_escrita = open(nome, mode)
+    def maior_sepal_length(self):
+        self.setosa_sepal_length = self.vetor_setosa[len(self.vetor_setosa) - 1]
+        self.versicolor_sepal_length = self.vetor_versicolor[len(self.vetor_versicolor) - 1]
+        self.virginica_sepal_length = self.vetor_virginica[len(self.vetor_virginica) - 1]
 
-    def escrever_saida(self):
-        for linha in self.arquivo_leitura:
-            self.arquivo_escrita.writelines()
+    def fecha_arquivo(self):
+        self.arquivo.close()
 
+    def abrir_escrita(self):
+        self.arquivo_saida = open('resultado.txt', 'wt')
+    
+    def escrever(self):
+        self.texto = [
+            'Quantidade de Iris-setosa: ', str(self.qtd_setosas),
+            '\nQuantidade de Iris-versicolor: ', str(self.qtd_versicolor),
+            '\nQuantidade de Iris-virginica: ', str(self.qtd_virginica),
+            '\nO maior sepal length da classe Iris-setosa tem: ', str(self.setosa_sepal_length), 'cm',
+            '\nO maior sepal length da classe Iris-versicolor tem: ', str(self.versicolor_sepal_length), 'cm',
+            '\nO maior sepal length da classe Iris-virginica tem: ', str(self.virginica_sepal_length), 'cm',
+        ]
+        self.arquivo_saida.writelines(self.texto)
+    
     def fechar_saida(self):
-        self.arquivo_escrita.close()
+        self.arquivo_saida.close()
 
-##################################################
+#############################
 teste = Arquivo()
-teste.abre_arquivo('iris.data', 'rt')
-teste.mostrar_linhas()
+teste.abre_arquivo()
+teste.transformar_em_vetor()
 teste.contar_setosa()
 teste.contar_versicolor()
 teste.contar_virginica()
+teste.maior_sepal_length()
 teste.fecha_arquivo()
+teste.abrir_escrita()
+teste.escrever()
+teste.fechar_saida()
